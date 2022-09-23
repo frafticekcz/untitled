bb4927fb111166143fdd186e8ca882273d075433
urban <juriban03@gmail.com>
2022-09-23 09:11:28 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("zadej 1. číslo");
+        Double num =Double.parseDouble(sc.nextLine());
+
+        System.out.println("zadej 2. číslo");
+        Double num2 =Double.parseDouble(sc.nextLine());
+
+        System.out.println(num+ " + " + num2 + " = " + (num+num2));
+        System.out.println(num+ " - " + num2 + " = " + (num-num2));
+        System.out.println(num+ " * " + num2 + " = " + (num*num2));
+        System.out.println(num+ " / " + num2 + " = " + (num/num2));
+
+
+    }
+
+
+
+}
