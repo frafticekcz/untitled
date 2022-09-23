584a139d7abdf7d14f79a186e31225288109e7ec
urban <juriban03@gmail.com>
2022-09-23 09:20:05 +0200

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,6 +19,7 @@ public class Main {
         System.out.println(num+ " * " + num2 + " = " + (num*num2));
         System.out.println(num+ " / " + num2 + " = " + (num/num2));
 
+        System.out.println("");
 
     }
 
